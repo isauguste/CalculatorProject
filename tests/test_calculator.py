aedb4b581946a94fe09974af5456b4a302dc773e
isauguste <isaugust25@gmail.com>
2025-06-14 15:38:51 -0400

    Defined multiply and division as well as fix docstring.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,23 +1,24 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    '''Test that addition function works'''
+    assert add(2, 2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    '''Test that subtraction function works'''
+    assert subtract(2, 2) == 0
 
-def test_multiplication(): 
-    '''Test that multiplication function works '''
+def test_multiplication():
+    '''Test that multiplication function works'''
     assert multiply(3, 4) == 12
 
-def test_division(): 
-    '''Test that division function works '''
+def test_division():
+    '''Test that division function works'''
     assert divide(10, 2) == 5
 
 def test_divide_by_zero():
+    '''Test that division by zero raises an error'''
     try:
         divide(5, 0)
     except ZeroDivisionError:
