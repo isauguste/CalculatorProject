eca3387131c53968fb7cd678a5c30989d7729f4c
isauguste <isaugust25@gmail.com>
2025-06-14 15:05:31 -0400

    Updated file to include test multiplication, division, and divide by 0

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,19 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
+
+def test_multiplication(): 
+    '''Test that multiplication function works '''
+    assert multiply(3, 4) == 12
+
+def test_division(): 
+    '''Test that division function works '''
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    try:
+        divide(5, 0)
+    except ZeroDivisionError:
+        assert True
+    else:
+        assert False
