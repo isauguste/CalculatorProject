f23672841ac9e2cf859ef116a1153aa6c2707492
isauguste <isaugust25@gmail.com>
2025-06-14 16:00:09 -0400

    Implemented staticmethod using Calculator

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,13 +1,40 @@
-def add(a,b):
-    return a + b
+class Calculation:
+    '''Performs a single calculation using instance methods'''
 
-def subtract(a,b):
-    return a - b
+    def __init__(self, a: float, b: float):
+        self.a = a
+        self.b = b
 
-def multiply(a, b):
-    return a * b
+    def add(self) -> float:
+        return self.a + self.b
 
-def divide(a, b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return a / b
+    def subtract(self) -> float:
+        return self.a - self.b
+
+    def multiply(self) -> float:
+        return self.a * self.b
+
+    def divide(self) -> float:
+        if self.b == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
+        return self.a / self.b
+
+
+class Calculator:
+    '''Uses static methods to call instance methods on Calculation'''
+
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        return Calculation(a, b).add()
+
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        return Calculation(a, b).subtract()
+
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        return Calculation(a, b).multiply()
+
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        return Calculation(a, b).divide()
